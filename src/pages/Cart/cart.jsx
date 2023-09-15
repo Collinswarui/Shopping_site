c9a0254a27979ed31354f842ed9e54ef9e789262
Collins Wanjiru <waruiwanjiru47@gmail.com>
2023-09-15 15:44:24 +0300

    Added logic to the continue shopping button

--- src/pages/Cart/cart.jsx
+++ src/pages/Cart/cart.jsx
@@ -1,30 +1,39 @@
-import React, { useContext } from 'react';
-import { PRODUCTS } from '../../product';
-import { ShopContext } from '../../context/shop-context';
-import {CartItem } from './cart-item';
-import './cart.css';
+import React, { useContext } from "react";
+import { ShopContext } from "../../context/shop-context";
+import { PRODUCTS } from "../../product";
+import { CartItem } from "./cart-item";
+import { useNavigate } from "react-router-dom";
 
+import "./cart.css";
 export const Cart = () => {
-    const { cartItems, getTotalCartAmount } = useContext(ShopContext);
-    const totalAmoun = getTotalCartAmount;
-    return (
-        <div className="cart">
-          <div>
-            <h1>Your Cart Items</h1>
-          </div>
-          <div className="cart">
-            {PRODUCTS.map((product) => {
-              if (cartItems[product.id] !== 0) {
-                return <CartItem data={product} />;
-              }
-            })}
-          </div>
+  const { cartItems, getTotalCartAmount, checkout } = useContext(ShopContext);
+  const totalAmount = getTotalCartAmount();
 
-          <div className='checkout'> 
-            <p>Subtotal: ${totalAmoun}</p>
-            <button>Continue Shopping</button>
-            <button>Checkout</button>
-          </div>
+  const navigate = useNavigate();
+
+  return (
+    <div className="cart">
+      <div>
+        <h1 className='heading'>Your Cart Items</h1>
+      </div>
+      <div className="cart">
+        {PRODUCTS.map((product) => {
+          if (cartItems[product.id] !== 0) {
+            return <CartItem data={product} />;
+          }
+        })}
+      </div>
+
+      {totalAmount > 0 ? (
+        <div className="checkout">
+          <p> Subtotal: ${totalAmount} </p>
+          <button onClick={() => navigate("/")}> Continue Shopping </button>
+          <button>Checkout</button>
+        
         </div>
-    )
-        }
+      ) : (
+        <h3 className='heading'> Your Shopping Cart is Empty</h3>
+      )}
+    </div>
+  );
+};
